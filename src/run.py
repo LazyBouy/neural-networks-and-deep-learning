96defa31505b84d31722719189c9bf486ba694b9
LazyBouy <i.gourabdeb@.com>
2023-09-01 17:09:54 +0200

    gd

--- src/run.py
+++ src/run.py
@@ -1,9 +1,15 @@
 import mnist_loader
-import network
+#import network as netw
+import network2 as netw
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper() 
 
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+"""
+net = netw.Network([784, 100, 10])
+net.SGD(training_data, 50, 10, 3.0, test_data=test_data)
 
- 
+"""
+
+net = netw.Network([784, 100, 10], cost=netw.CrossEntropyCost)
+net.large_weight_initializer()
+net.SGD(training_data, 50, 10, 3.0, evaluation_data=test_data, monitor_evaluation_accuracy=True)
